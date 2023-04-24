6691356f8b32d0a6adf0b2a4c4f5e52da814fd15
Liu Guoqing <76424760+liuguoqing000@users.noreply.github.com>
2023-04-24 15:22:58 +0800

    Update demo.java

--- src/com/demo/demo.java
+++ src/com/demo/demo.java
@@ -2,6 +2,7 @@ package com.demo;
 
 public class demo {
     public static void main(String[] args) {
-        System.out.print("test");
+        System.out.println("test");
+        System.out.print("pull test");
     }
 }
