b0b1bf2593e7267eaabbcb40d597e73bbe5047e3
liuguoqing000 <3101359583@qq.com>
2023-05-09 08:59:35 +0800

    test

--- src/com/demo/demo.java
+++ src/com/demo/demo.java
@@ -1,21 +1,8 @@
 package com.demo;
 
-import java.util.Objects;
-import java.util.Scanner;
-
 public class demo {
     public static void main(String[] args) {
         int b = 0;
-        int a = 10;
-        int c = 10;
-        int d = 0;
-        try {
-            System.out.println(a/b);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            b = c;
-            System.out.println(a/b);
-        }
     }
 }
 
